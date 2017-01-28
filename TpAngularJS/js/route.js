c4ca04da9982bcafc06957195199653e0e60b31b
sbenbelkacem <s_benbelkacem@esi.dz>
2017-01-28 21:25:48 +0100

    Delete route.js

--- TpAngularJS/js/route.js
+++
@@ -1,9 +0,0 @@
-meteoApp.config(['$routeProvider',
-  function($routeProvider){
-    $routeProvider.when('/home',{
-      controller:'MainController', templateUrl:'html/home.html'
-    })
-    .otherwise({
-        redirectTo:'/home'    
-    });
-  }]);
